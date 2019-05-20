369f6555411b239b1aef37d74b1b42084d6f99f9
Alex Kesling <alex@kesling.co>
2019-05-20 15:18:17 -0400

    Change to remove unsafe block.

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,6 @@ use std::io::{self, Read};
 const HEAPSIZE: usize = 30000;
 const STACKSIZE: usize = 30000;
 
-static mut DATA: [i8; HEAPSIZE] = [0; HEAPSIZE];
-static mut CODE: [u8; HEAPSIZE] = [0; HEAPSIZE];
-static mut JUMPS: [usize; HEAPSIZE] = [0; HEAPSIZE];
-static mut STACK: [usize; STACKSIZE] = [0; STACKSIZE];
-
 fn read_char() -> u8 {
     std::io::stdin()
         .bytes() 
@@ -17,71 +12,73 @@ fn read_char() -> u8 {
 }
 
 fn main() -> io::Result<()> {
-    // TODO(Alex): Make this all safe.
-    unsafe {
-        let mut buffer = String::new();
-        io::stdin().read_to_string(&mut buffer)?;
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
 
-        let code_length;
-        {
-            // Precompute jumps and remove comments
-            let mut stack_index: usize = 0;
-            let mut code_counter = 0;
-            for code_char in buffer.chars() {
-                match code_char {
-                    '<' | '>'| '+'| '-'| '.'| ',' => {
-                        CODE[code_counter] = code_char as u8;
-                        code_counter = code_counter + 1;
-                    },
-                    '[' => {
-                        STACK[stack_index] = code_counter;
-                        stack_index = stack_index + 1;
+    let mut data_segment: [i8; HEAPSIZE] = [0; HEAPSIZE];
+    let mut code_segment: [u8; HEAPSIZE] = [0; HEAPSIZE];
+    let mut jump_lookup: [usize; HEAPSIZE] = [0; HEAPSIZE];
+    let mut stack_lookup: [usize; STACKSIZE] = [0; STACKSIZE];
 
-                        CODE[code_counter] = code_char as u8;
-                        code_counter = code_counter + 1;
-                    },
-                    ']' => {
-                        stack_index = stack_index - 1;
-
-                        JUMPS[STACK[stack_index]] = code_counter;
-                        JUMPS[code_counter] = STACK[stack_index];
-
-                        CODE[code_counter] = code_char as u8;
-                        code_counter = code_counter + 1;
-                    },
-                    _ => (),
-                }
-            }
-            code_length = code_counter + 1;
-        }
-
-        // Execute
-        let mut code_ptr = 0;
-        let mut data_ptr = 0;
-        while code_ptr < code_length {
-            match CODE[code_ptr].clone() as char {
-                '<' => data_ptr = data_ptr - 1,
-                '>' => data_ptr = data_ptr + 1,
-                '+' => DATA[data_ptr] = DATA[data_ptr] + 1,
-                '-' => DATA[data_ptr] = DATA[data_ptr] - 1,
+    let code_length;
+    {
+        // Precompute jumps and remove comments
+        let mut stack_index: usize = 0;
+        let mut code_counter = 0;
+        for code_char in buffer.chars() {
+            match code_char {
+                '<' | '>'| '+'| '-'| '.'| ',' => {
+                    code_segment[code_counter] = code_char as u8;
+                    code_counter = code_counter + 1;
+                },
                 '[' => {
-                    if DATA[data_ptr] == 0 {
-                        code_ptr = JUMPS[code_ptr];
-                    }
+                    stack_lookup[stack_index] = code_counter;
+                    stack_index = stack_index + 1;
+
+                    code_segment[code_counter] = code_char as u8;
+                    code_counter = code_counter + 1;
                 },
                 ']' => {
-                    if DATA[data_ptr] != 0 {
-                        code_ptr = JUMPS[code_ptr];
-                    }
+                    stack_index = stack_index - 1;
+
+                    jump_lookup[stack_lookup[stack_index]] = code_counter;
+                    jump_lookup[code_counter] = stack_lookup[stack_index];
+
+                    code_segment[code_counter] = code_char as u8;
+                    code_counter = code_counter + 1;
                 },
-                '.' => {
-                    print!("{}", DATA[data_ptr] as u8 as char);
-                },
-                ',' => DATA[data_ptr] = read_char() as i8,
                 _ => (),
             }
-            code_ptr = code_ptr + 1;
         }
+        code_length = code_counter + 1;
+    }
+
+    // Execute
+    let mut code_ptr = 0;
+    let mut data_ptr = 0;
+    while code_ptr < code_length {
+        match code_segment[code_ptr].clone() as char {
+            '<' => data_ptr = data_ptr - 1,
+            '>' => data_ptr = data_ptr + 1,
+            '+' => data_segment[data_ptr] = data_segment[data_ptr] + 1,
+            '-' => data_segment[data_ptr] = data_segment[data_ptr] - 1,
+            '[' => {
+                if data_segment[data_ptr] == 0 {
+                    code_ptr = jump_lookup[code_ptr];
+                }
+            },
+            ']' => {
+                if data_segment[data_ptr] != 0 {
+                    code_ptr = jump_lookup[code_ptr];
+                }
+            },
+            '.' => {
+                print!("{}", data_segment[data_ptr] as u8 as char);
+            },
+            ',' => data_segment[data_ptr] = read_char() as i8,
+            _ => (),
+        }
+        code_ptr = code_ptr + 1;
     }
 
     Ok(())
