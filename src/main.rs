33cbcdc472a638e34abd922948ce5cee6bfc006f
Alex Kesling <alex@kesling.co>
2019-05-21 13:33:28 -0400

    Convert from chars to an Enum for instruction repr

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,35 @@ use std::io::{self, Read};
 const HEAPSIZE: usize = 30000;
 const STACKSIZE: usize = 30000;
 
+#[derive(Copy, Clone)]
+enum Instruction {
+    Backward,
+    Forward,
+    Subtract,
+    Add,
+    Land,
+    Jump,
+    Write,
+    Read,
+    Nop,
+}
+
+impl Instruction {
+    fn get_instruction(character: char) -> Instruction {
+        match character {
+            '<' => Instruction::Backward,
+            '>' => Instruction::Forward,
+            '-' => Instruction::Subtract,
+            '+' => Instruction::Add,
+            '[' => Instruction::Land,
+            ']' => Instruction::Jump,
+            '.' => Instruction::Write,
+            ',' => Instruction::Read,
+            _ => Instruction::Nop,
+        }
+    }
+}
+
 fn read_char() -> u8 {
     std::io::stdin()
         .bytes() 
@@ -16,7 +45,7 @@ fn main() -> io::Result<()> {
     io::stdin().read_to_string(&mut buffer)?;
 
     let mut data_segment: [i8; HEAPSIZE] = [0; HEAPSIZE];
-    let mut code_segment: [u8; HEAPSIZE] = [0; HEAPSIZE];
+    let mut code_segment: [Instruction; HEAPSIZE] = [Instruction::Nop; HEAPSIZE];
     let mut jump_lookup: [usize; HEAPSIZE] = [0; HEAPSIZE];
     let mut stack_lookup: [usize; STACKSIZE] = [0; STACKSIZE];
 
@@ -28,14 +57,14 @@ fn main() -> io::Result<()> {
         for code_char in buffer.chars() {
             match code_char {
                 '<' | '>'| '+'| '-'| '.'| ',' => {
-                    code_segment[code_counter] = code_char as u8;
+                    code_segment[code_counter] = Instruction::get_instruction(code_char);
                     code_counter = code_counter + 1;
                 },
                 '[' => {
                     stack_lookup[stack_index] = code_counter;
                     stack_index = stack_index + 1;
 
-                    code_segment[code_counter] = code_char as u8;
+                    code_segment[code_counter] = Instruction::get_instruction(code_char);
                     code_counter = code_counter + 1;
                 },
                 ']' => {
@@ -44,7 +73,7 @@ fn main() -> io::Result<()> {
                     jump_lookup[stack_lookup[stack_index]] = code_counter;
                     jump_lookup[code_counter] = stack_lookup[stack_index];
 
-                    code_segment[code_counter] = code_char as u8;
+                    code_segment[code_counter] = Instruction::get_instruction(code_char);
                     code_counter = code_counter + 1;
                 },
                 _ => (),
@@ -57,26 +86,26 @@ fn main() -> io::Result<()> {
     let mut code_ptr = 0;
     let mut data_ptr = 0;
     while code_ptr < code_length {
-        match code_segment[code_ptr].clone() as char {
-            '<' => data_ptr = data_ptr - 1,
-            '>' => data_ptr = data_ptr + 1,
-            '+' => data_segment[data_ptr] = data_segment[data_ptr] + 1,
-            '-' => data_segment[data_ptr] = data_segment[data_ptr] - 1,
-            '[' => {
+        match code_segment[code_ptr] {
+            Instruction::Backward => data_ptr = data_ptr - 1,
+            Instruction::Forward => data_ptr = data_ptr + 1,
+            Instruction::Add => data_segment[data_ptr] = data_segment[data_ptr] + 1,
+            Instruction::Subtract => data_segment[data_ptr] = data_segment[data_ptr] - 1,
+            Instruction::Land => {
                 if data_segment[data_ptr] == 0 {
                     code_ptr = jump_lookup[code_ptr];
                 }
             },
-            ']' => {
+            Instruction::Jump => {
                 if data_segment[data_ptr] != 0 {
                     code_ptr = jump_lookup[code_ptr];
                 }
             },
-            '.' => {
+            Instruction::Write => {
                 print!("{}", data_segment[data_ptr] as u8 as char);
             },
-            ',' => data_segment[data_ptr] = read_char() as i8,
-            _ => (),
+            Instruction::Read => data_segment[data_ptr] = read_char() as i8,
+            Instruction::Nop => (),
         }
         code_ptr = code_ptr + 1;
     }
