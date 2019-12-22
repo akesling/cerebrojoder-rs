1bdb5f2ab97e6759f3db6177a20ffebfc1998f86
Alex Kesling <alex@kesling.co>
2019-12-22 17:34:33 -0500

    Conditionally compile different mains based on desired backend

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+#[cfg(feature = "interpreter")]
 use std::io::Read;
 
+#[cfg(feature = "interpreter")]
 use ::cerebrojoder_rs::{BfInterpreter, Executor, Parser};
 
+#[cfg(feature = "interpreter")]
 fn main() -> std::io::Result<()> {
     let mut buffer = String::new();
     std::io::stdin().read_to_string(&mut buffer)?;
@@ -9,4 +12,11 @@ fn main() -> std::io::Result<()> {
     let module = BfInterpreter::parse(&buffer);
 
     BfInterpreter::execute(&module)
+}
+
+#[cfg(feature = "wasmer-backend")]
+fn main() -> std::io::Result<()> {
+    println!("Not yet implemented!");
+
+    Ok(())
 }
