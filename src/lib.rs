9deed06ee0d7e757be5a7f02f693877ad6d3a97a
Alex Kesling <alex@kesling.co>
2019-12-22 17:10:18 -0500

    Factor out parser into a new type and impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+
 pub const HEAPSIZE: usize = 30000;
 pub const CODESIZE: usize = HEAPSIZE;
 pub const STACKSIZE: usize = 30000;
@@ -37,3 +39,99 @@ pub struct Module<InstructionType> {
     pub code_length: usize,
     pub jump_lookup: [usize; HEAPSIZE],
 }
+
+pub trait Parser<TargetInstructionType> {
+    fn parse(buffer: &str) -> Module<TargetInstructionType>;
+}
+
+#[inline]
+pub fn read_char() -> u8 {
+    std::io::stdin()
+        .bytes()
+        .next()
+        .and_then(|result| result.ok())
+        .map(|byte| byte as u8).unwrap()
+}
+
+#[inline]
+fn take_one(buffer: &str) -> (&str, &str) {
+    (&buffer[0..1], &buffer[1..])
+}
+
+#[inline]
+fn take_all(buffer: &str, character: char) -> (&str, &str) {
+    let to_compare = character.to_string();
+    let mut partition = 1;
+    while buffer[partition-1..partition] == to_compare && partition < std::u8::MAX as usize {
+        partition = partition + 1;
+    }
+    partition = partition - 1;
+    (&buffer[0..partition], &buffer[partition..])
+}
+
+
+pub struct BfInterpreter;
+impl Parser<Instruction> for BfInterpreter {
+    fn parse(buffer: &str) -> Module<Instruction> {
+        let mut code_segment: [Instruction; HEAPSIZE] = [Instruction::Nop; HEAPSIZE];
+        let mut jump_lookup: [usize; HEAPSIZE] = [0; HEAPSIZE];
+        let mut stack_lookup: [usize; STACKSIZE] = [0; STACKSIZE];
+
+        let code_length;
+        {
+            // Precompute jumps and remove comments
+            let mut stack_index: usize = 0;
+            let mut code_counter = 0;
+
+            // TODO(Alex): Is there a better way to do this?  Maybe a stream of some form?
+            let mut tail: &str = buffer;
+            while !tail.is_empty() {
+                let code_char = tail.chars().next().unwrap();
+                match code_char {
+                    '<' | '>'| '+'| '-'| '.'| ',' => {
+                        let (_head, _tail) = take_all(tail, code_char);
+                        tail = _tail;
+
+                        code_segment[code_counter] = Instruction::get_instruction(code_char, _head.len() as u8);
+                        code_counter = code_counter + 1;
+                    },
+                    // TODO(Alex): Investigate simple loop unrolling.
+                    '[' => {
+                        stack_lookup[stack_index] = code_counter;
+                        stack_index = stack_index + 1;
+
+                        let (_head, _tail) = take_one(tail);
+                        tail = _tail;
+
+                        code_segment[code_counter] = Instruction::get_instruction(code_char, 1);
+                        code_counter = code_counter + 1;
+                    },
+                    ']' => {
+                        stack_index = stack_index - 1;
+
+                        jump_lookup[stack_lookup[stack_index]] = code_counter;
+                        jump_lookup[code_counter] = stack_lookup[stack_index];
+
+                        let (_head, _tail) = take_one(tail);
+                        tail = _tail;
+
+                        code_segment[code_counter] = Instruction::get_instruction(code_char, 1);
+                        code_counter = code_counter + 1;
+                    },
+                    _ => {
+                        let (_head, _tail) = take_one(tail);
+                        tail = _tail;
+                    },
+                }
+            }
+            code_length = code_counter + 1;
+        }
+
+        Module {
+            code_segment,
+            code_length,
+            jump_lookup,
+        }
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,8 @@
-use ::cerebrojoder_rs::{Instruction, Module, HEAPSIZE, STACKSIZE};
+use std::io::Read;
 
-use std::io::{self, Read};
+use ::cerebrojoder_rs::{Instruction, Module, HEAPSIZE, read_char, BfInterpreter, Parser};
 
-#[inline]
-fn read_char() -> u8 {
-    std::io::stdin()
-        .bytes() 
-        .next()
-        .and_then(|result| result.ok())
-        .map(|byte| byte as u8).unwrap()
-}
-
-#[inline]
-fn take_one(buffer: &str) -> (&str, &str) {
-    (&buffer[0..1], &buffer[1..])
-}
-
-#[inline]
-fn take_all(buffer: &str, character: char) -> (&str, &str) {
-    let to_compare = character.to_string();
-    let mut partition = 1;
-    while buffer[partition-1..partition] == to_compare && partition < std::u8::MAX as usize {
-        partition = partition + 1;
-    }
-    partition = partition - 1;
-    (&buffer[0..partition], &buffer[partition..])
-}
-
-fn parse_and_compile(buffer: &str) -> Module<Instruction> {
-    let mut code_segment: [Instruction; HEAPSIZE] = [Instruction::Nop; HEAPSIZE];
-    let mut jump_lookup: [usize; HEAPSIZE] = [0; HEAPSIZE];
-    let mut stack_lookup: [usize; STACKSIZE] = [0; STACKSIZE];
-
-    let code_length;
-    {
-        // Precompute jumps and remove comments
-        let mut stack_index: usize = 0;
-        let mut code_counter = 0;
-
-        // TODO(Alex): Is there a better way to do this?  Maybe a stream of some form?
-        let mut tail: &str = buffer;
-        while !tail.is_empty() {
-            let code_char = tail.chars().next().unwrap();
-            match code_char {
-                '<' | '>'| '+'| '-'| '.'| ',' => {
-                    let (_head, _tail) = take_all(tail, code_char);
-                    tail = _tail;
-
-                    code_segment[code_counter] = Instruction::get_instruction(code_char, _head.len() as u8);
-                    code_counter = code_counter + 1;
-                },
-                // TODO(Alex): Investigate simple loop unrolling.
-                '[' => {
-                    stack_lookup[stack_index] = code_counter;
-                    stack_index = stack_index + 1;
-
-                    let (_head, _tail) = take_one(tail);
-                    tail = _tail;
-
-                    code_segment[code_counter] = Instruction::get_instruction(code_char, 1);
-                    code_counter = code_counter + 1;
-                },
-                ']' => {
-                    stack_index = stack_index - 1;
-
-                    jump_lookup[stack_lookup[stack_index]] = code_counter;
-                    jump_lookup[code_counter] = stack_lookup[stack_index];
-
-                    let (_head, _tail) = take_one(tail);
-                    tail = _tail;
-
-                    code_segment[code_counter] = Instruction::get_instruction(code_char, 1);
-                    code_counter = code_counter + 1;
-                },
-                _ => {
-                    let (_head, _tail) = take_one(tail);
-                    tail = _tail;
-                },
-            }
-        }
-        code_length = code_counter + 1;
-    }
-
-    Module {
-        code_segment,
-        code_length,
-        jump_lookup,
-    }
-}
-
-fn execute(module: &Module<Instruction>) -> io::Result<()> {
+fn execute(module: &Module<Instruction>) -> std::io::Result<()> {
     // 8bit signed rollover is necessary for the mandelbrot implementation, so we use i8 for the
     // data segment.
     let mut data_segment: [i8; HEAPSIZE] = [0; HEAPSIZE];
@@ -125,11 +38,11 @@ fn execute(module: &Module<Instruction>) -> io::Result<()> {
     Ok(())
 }
 
-fn main() -> io::Result<()> {
+fn main() -> std::io::Result<()> {
     let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer)?;
+    std::io::stdin().read_to_string(&mut buffer)?;
 
-    let module = parse_and_compile(&buffer);
+    let module = BfInterpreter::parse(&buffer);
 
     execute(&module)
 }
