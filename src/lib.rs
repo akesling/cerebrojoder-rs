c229fa7fc3cb436eae55d58c50b5eb35bc50da88
Alex Kesling <alex@kesling.co>
2019-12-22 17:13:13 -0500

    Factor out executor as a new trait and impl

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,10 @@ pub trait Parser<TargetInstructionType> {
     fn parse(buffer: &str) -> Module<TargetInstructionType>;
 }
 
+pub trait Executor<TargetInstructionType> {
+    fn execute(module: &Module<TargetInstructionType>) -> std::io::Result<()>;
+}
+
 #[inline]
 pub fn read_char() -> u8 {
     std::io::stdin()
@@ -133,5 +137,45 @@ impl Parser<Instruction> for BfInterpreter {
             code_length,
             jump_lookup,
         }
+    }
+}
+
+impl Executor<Instruction> for BfInterpreter {
+    fn execute(module: &Module<Instruction>) -> std::io::Result<()> {
+        // 8bit signed rollover is necessary for the mandelbrot implementation, so we use i8 for the
+        // data segment.
+        let mut data_segment: [i8; HEAPSIZE] = [0; HEAPSIZE];
+
+        // Execute
+        let mut code_ptr: usize = 0;
+        let mut data_ptr: usize = 0;
+        while code_ptr < module.code_length {
+            match module.code_segment[code_ptr] {
+                Instruction::Backward(v) => data_ptr = data_ptr - v as usize,
+                Instruction::Forward(v) => data_ptr = data_ptr + v as usize,
+                Instruction::Add(v) => data_segment[data_ptr] = data_segment[data_ptr] + v as i8,
+                Instruction::Subtract(v) => {
+                    data_segment[data_ptr] = data_segment[data_ptr] - v as i8
+                }
+                Instruction::Land => {
+                    if data_segment[data_ptr] == 0 {
+                        code_ptr = module.jump_lookup[code_ptr];
+                    }
+                }
+                Instruction::Jump => {
+                    if data_segment[data_ptr] != 0 {
+                        code_ptr = module.jump_lookup[code_ptr];
+                    }
+                }
+                Instruction::Write => {
+                    print!("{}", data_segment[data_ptr] as u8 as char);
+                }
+                Instruction::Read => data_segment[data_ptr] = read_char() as i8,
+                Instruction::Nop => (),
+            }
+            code_ptr = code_ptr + 1;
+        }
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,6 @@
 use std::io::Read;
 
-use ::cerebrojoder_rs::{read_char, BfInterpreter, Instruction, Module, Parser, HEAPSIZE};
-
-fn execute(module: &Module<Instruction>) -> std::io::Result<()> {
-    // 8bit signed rollover is necessary for the mandelbrot implementation, so we use i8 for the
-    // data segment.
-    let mut data_segment: [i8; HEAPSIZE] = [0; HEAPSIZE];
-
-    // Execute
-    let mut code_ptr: usize = 0;
-    let mut data_ptr: usize = 0;
-    while code_ptr < module.code_length {
-        match module.code_segment[code_ptr] {
-            Instruction::Backward(v) => data_ptr = data_ptr - v as usize,
-            Instruction::Forward(v) => data_ptr = data_ptr + v as usize,
-            Instruction::Add(v) => data_segment[data_ptr] = data_segment[data_ptr] + v as i8,
-            Instruction::Subtract(v) => data_segment[data_ptr] = data_segment[data_ptr] - v as i8,
-            Instruction::Land => {
-                if data_segment[data_ptr] == 0 {
-                    code_ptr = module.jump_lookup[code_ptr];
-                }
-            }
-            Instruction::Jump => {
-                if data_segment[data_ptr] != 0 {
-                    code_ptr = module.jump_lookup[code_ptr];
-                }
-            }
-            Instruction::Write => {
-                print!("{}", data_segment[data_ptr] as u8 as char);
-            }
-            Instruction::Read => data_segment[data_ptr] = read_char() as i8,
-            Instruction::Nop => (),
-        }
-        code_ptr = code_ptr + 1;
-    }
-
-    Ok(())
-}
+use ::cerebrojoder_rs::{BfInterpreter, Executor, Parser};
 
 fn main() -> std::io::Result<()> {
     let mut buffer = String::new();
@@ -44,5 +8,5 @@ fn main() -> std::io::Result<()> {
 
     let module = BfInterpreter::parse(&buffer);
 
-    execute(&module)
+    BfInterpreter::execute(&module)
 }
